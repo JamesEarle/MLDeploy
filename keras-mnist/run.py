b03552b092e3223f141fbdfc854a4085556e5cf6
James Earle <jaearle@microsoft.com>
2018-11-13 17:41:36 -0800

    trying sklearn model, still fails on deploy but successfully runs locally

--- keras-mnist/run.py
+++ keras-mnist/run.py
@@ -1,11 +1,13 @@
 import os
 from azureml.core import Workspace, Experiment, Run, ScriptRunConfig, RunConfiguration
+from sklearn.externals import joblib
 
 # Import the model defined and trained in our train.py script.
-import train
+# import train
 
 ws_name = "mldeployworkspace"
-exp_name = dir_name = "keras-mnist" 
+# exp_name = dir_name = "keras-mnist" 
+exp_sklearn_name = dir_name = "sklearn-mnist" 
 sub_key = os.getenv("AZURE_SUBSCRIPTION")
 resource_group = "MLDeploy"
 location = "westus2"
@@ -14,10 +16,11 @@ location = "westus2"
 ws = Workspace.get(ws_name, subscription_id=sub_key)
 
 # Get experiment
-exp = Experiment(workspace=ws, name=exp_name)
+exp = Experiment(workspace=ws, name=exp_sklearn_name)
 print(exp)
 
-model, metrics = train.build_train_model()
+# model, metrics = train.build_train_model()
+model, metrics = train.build_train_sklearn_model()
 
 # Create a new run
 config = ScriptRunConfig(source_directory='.', script='train.py', run_config=RunConfiguration())
@@ -25,9 +28,12 @@ run = exp.submit(config)
 
 # Create an outputs directory
 os.makedirs('outputs', exist_ok=True)
-model.save("./outputs/keras-mnist-model.h5")
 
-run.log_list("Metric labels: ", model.metrics_names)
-run.log_list("Metrics values: ", metrics)
+# model.save("./outputs/keras-mnist-model.h5") # Keras save model
+joblib.dump(value=model, filename='./outputs/sklearn_mnist_model.pkl') # sklearn save model
 
-print(model.name, metrics)
+# run.log_list("Metric labels: ", model.metrics_names) # keras metrics names
+run.log("sklearn-mnist: accuracy =", metrics) # sklearn accuracy 
+# run.log_list("Metrics values: ", metrics)
+
+print("sklearn-mnist", metrics)
--- keras-mnist/train.py
+++ keras-mnist/train.py
@@ -1,41 +1,102 @@
+# from azureml.core import Run
+
+# def build_train_model():
+
+#     # Loading sample data from within Keras, here you would link to your actual data set locally or in Azure
+#     (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+#     # Simple dense model
+#     model = models.Sequential()
+#     model.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+#     model.add(layers.Dense(10, activation='softmax'))
+
+#     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
+
+#     # Flatten image data, current shape is (nsamples, 28, 28), we want (nsamples, 28*28,)
+#     X_train = X_train.reshape((len(X_train), 28*28))
+#     X_test = X_test.reshape((len(X_test), 28*28))
+
+#     # Then we normalize our channel data.
+#     X_train = X_train.astype('float32') / 255
+#     X_test = X_test.astype('float32') / 255
+
+#     # One-hot encoding training and testing labels
+#     y_train = to_categorical(y_train)
+#     y_test = to_categorical(y_test)
+
+#     # Train on our data
+#     model.fit(X_train, y_train, epochs=5, batch_size=128)
+
+#     # Evaluate loss and accuracy
+#     metrics = model.evaluate(X_test, y_test)
+
+#     return model, metrics
+
+# def build_train_sklearn_model(): 
+from sklearn.linear_model import LogisticRegression
+from azureml.core import ScriptRunConfig
+from azureml.core import Run
+from azureml.core import Workspace
+from azureml.core import RunConfiguration
+from azureml.core import Experiment
+
 import os
+import numpy as np
 
 from keras import models
 from keras import layers
-
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
-from azureml.core import Run
+sub_key = os.getenv("AZURE_SUBSCRIPTION")
+ws_name = "mldeployworkspace"
+skl = False
+exp_name = "sklearn-mnist" if skl else "keras-mnist"
 
-def build_train_model():
+# Get workspace
+ws = Workspace.get(ws_name, subscription_id=sub_key)
 
-    # Loading sample data from within Keras, here you would link to your actual data set locally or in Azure
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Get experiment
+exp = Experiment(workspace=ws, name=exp_name)
+# print(exp)
 
-    # Simple dense model
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Flatten image data, current shape is (nsamples, 28, 28), we want (nsamples, 28*28,)
+X_train = X_train.reshape((len(X_train), 28*28))
+X_test = X_test.reshape((len(X_test), 28*28))
+
+# Then we normalize our channel data.
+X_train = X_train.astype('float32') / 255
+X_test = X_test.astype('float32') / 255
+
+config = ScriptRunConfig(source_directory='.', script='train.py', run_config=RunConfiguration())
+run = exp.submit(config)
+# print(run.experiment)
+
+if(skl): # temporary, abstract out later
+    clf = LogisticRegression(random_state=42)
+    print(X_train.shape, y_train.shape)
+    clf.fit(X_train, y_train)
+    acc = np.average(clf.predict(X_test) == y_test)
+    print(acc)
+    run.log("accuracy", np.float(acc))
+else: #keras
+    # One-hot encoding training and testing labels
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
     model = models.Sequential()
     model.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
     model.add(layers.Dense(10, activation='softmax'))
 
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
 
-    # Flatten image data, current shape is (nsamples, 28, 28), we want (nsamples, 28*28,)
-    X_train = X_train.reshape((len(X_train), 28*28))
-    X_test = X_test.reshape((len(X_test), 28*28))
-
-    # Then we normalize our channel data.
-    X_train = X_train.astype('float32') / 255
-    X_test = X_test.astype('float32') / 255
-
-    # One-hot encoding training and testing labels
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
-
-    # Train on our data
     model.fit(X_train, y_train, epochs=5, batch_size=128)
 
-    # Evaluate loss and accuracy
-    metrics = model.evaluate(X_test, y_test)
+    # os.makedirs('outputs', exist_ok=True)
+    # model.save("./outputs/keras-mnist-model.h5")
 
-    return model, metrics
+    run.log_list("Metric labels: ", model.metrics_names)
+    run.log_list("accuracy", model.evaluate(X_test, y_test))
+# build_train_sklearn_model()
